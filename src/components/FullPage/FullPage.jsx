9473f96c6a62a3ae97b4505c2d323ae27ebe6e7e
Vincent Flattot <v.flattot@gmail.com>
2023-05-05 15:12:54 +0200

    feat(fullpage): add scroller component

---
+++ src/components/FullPage/FullPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+import "./FullPage.css";
+const anchors = [ "firstPage", "secondPage", "thirdPage" ];
+
+const FullPage = () => {
+    return (
+        <ReactFullpage
+            anchors={anchors}
+            navigation
+            navigationTooltips={anchors}
+            navigat
+            sectionsColor={[ "#7fff00", "#00ffff", "#29ab87" ]}
+            onLeave={( origin, destination, direction ) => {
+                console.log( "onLeave event", { origin, destination, direction } );
+            }}
+            render={( { state, fullpageApi } ) => {
+                console.log( "render prop change", state, fullpageApi );
+
+                return (
+                    <div>
+                        <div className="section"><h3>Section 1</h3></div>
+                        <div className="section"><h3>Section 2</h3></div>
+                        <div className="section"><h3>Section 3</h3></div>
+                    </div>
+                );
+            }}
+        />
+    );
+};
+export default FullPage;
