3e2b582ddfb97bae3104c97ab4921358a510a687
Vincent Flattot <v.flattot@gmail.com>
2023-05-10 22:31:48 +0200

    feat(CallPerTemperatureChart): add v0 of bar plot

--- src/components/slides/TemperatureSlide/CallsPerTemperatureChart/CallsPerTemperatureChart.jsx
+++ src/components/slides/TemperatureSlide/CallsPerTemperatureChart/CallsPerTemperatureChart.jsx
@@ -2,85 +2,69 @@ import React from "react";
 import { useD3 } from "../../../../hook/useD3";
 import * as d3 from "d3";
 import PropTypes from "prop-types";
+import "./CallsPerTemperatureChart.css";
+
 
 export const CallsPerTemperatureChart = ( { data } ) => {
     const ref = useD3(
         ( svg ) => {
+            
             const height = 500;
-            const width = 500;
-            const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-  
-            const x = d3
+            const width = 1150;
+            const margin = { top: 20, right: 30, bottom: 30, left: 30 };
+            
+            var x = d3
                 .scaleBand()
-                .domain( data.map( ( d ) => d.title ) )
-                .rangeRound( [ margin.left, width - margin.right ] )
-                .padding( 0.1 );
-  
-            const y1 = d3
+                .domain( data.map( d => {return d.title; } ) )
+                .range( [ 0, width ] )
+                .padding( 0.2 );
+
+            const y = d3
                 .scaleLinear()
                 .domain( [ 0, d3.max( data, ( d ) => d.calls_count ) ] )
-                .rangeRound( [ height - margin.bottom, margin.top ] );
+                .rangeRound( [ height, margin.top ] );
   
-            const xAxis = ( g ) =>
-                g.attr( "transform", `translate(0,${height - margin.bottom})` ).call(
-                    d3
-                        .axisBottom( x )
-                        .tickValues(
-                            d3
-                                .ticks( ...d3.extent( x.domain() ), width / 40 )
-                                .filter( ( v ) => x( v ) !== undefined )
-                        )
-                        .tickSizeOuter( 0 )
-                );
-  
-            const y1Axis = ( g ) =>
-                g
-                    .attr( "transform", `translate(${margin.left},0)` )
-                    .style( "color", "steelblue" )
-                    .call( d3.axisLeft( y1 ).ticks( null, "s" ) )
-                    .call( ( g ) => g.select( ".domain" ).remove() )
-                    .call( ( g ) =>
-                        g
-                            .append( "text" )
-                            .attr( "x", -margin.left )
-                            .attr( "y", 10 )
-                            .attr( "fill", "currentColor" )
-                            .attr( "text-anchor", "start" )
-                            .text( data.y1 )
-                    );
-  
-            svg.select( ".x-axis" ).call( xAxis );
-            svg.select( ".y-axis" ).call( y1Axis );
-  
-            svg
-                .select( ".plot-area" )
-                .attr( "fill", "steelblue" )
-                .selectAll( ".bar" )
+            svg.append( "g" )
+                .call( d3.axisBottom( x ) )
+                .attr( "transform", "translate(" + margin.left + "," + ( height + height / 20 ) + ")" )
+                .selectAll( "text" )
+                .attr( "transform", "translate(-10,0)rotate(-45)" )
+                .style( "text-anchor", "end" );
+
+            svg.append( "g" )
+                .call( d3.axisLeft( y ) )
+                .attr( "transform", "translate(" + margin.left + "," + height / 20 + ")" );
+            svg.selectAll( "mybar" )
                 .data( data )
-                .join( "rect" )
-                .attr( "class", "bar" )
-                .attr( "x", ( d ) => x( d.title ) )
+                .enter()
+                .append( "rect" )
+                .attr( "x", d => { return x( d.title ); } )
                 .attr( "width", x.bandwidth() )
-                .attr( "y", ( d ) => y1( d.calls_count ) )
-                .attr( "height", ( d ) => y1( 0 ) - y1( d.calls_count ) );
+                .attr( "fill", "blue" )
+            // no bar at the beginning thus:
+                .attr( "y", d => { return y( d.calls_count ); } )
+                .attr( "height", d => { return height - y( d.calls_count ); } )
+                .attr( "transform", "translate(" + margin.left + "," + height / 20 + ")" );
+
         },
         [ data.length ]
     );
   
     return (
-        <svg
-            ref={ref}
-            style={{
-                height: 500,
-                width: "100%",
-                marginRight: "0px",
-                marginLeft: "0px",
-            }}
-        >
-            <g className="plot-area" />
-            <g className="x-axis" />
-            <g className="y-axis" />
-        </svg>
+        <div className="chart">
+            <svg
+                ref={ref}
+                style={{
+                    height: "100%",
+                    width: "100%",
+                    marginRight: "auto",
+                    marginLeft: "auto",
+                }}
+            >
+                <g className="plot-area" />
+
+            </svg>
+        </div>
     );
 };
-CallsPerTemperatureChart.propTypes = { data: PropTypes.arrayOf( PropTypes.shape( { "temperature": PropTypes.number, "title": PropTypes.number, "calls_count": PropTypes.number } ) ) };
+CallsPerTemperatureChart.propTypes = { data: PropTypes.arrayOf( PropTypes.shape( { "temperature": PropTypes.number, "title": PropTypes.string, "calls_count": PropTypes.number } ) ) };
