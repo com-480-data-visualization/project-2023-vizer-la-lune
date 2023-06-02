dc82791ec95ddc7cfc625ba0209bb2314a5c9e57
delacombazlinda <delacombaz.linda@gmail.com>
2023-06-02 09:48:43 +0200

    first draft of the button

--- src/components/slides/GlobalMapSlide/GlobalMapYearButton/GlobalMapYearButton.jsx
+++ src/components/slides/GlobalMapSlide/GlobalMapYearButton/GlobalMapYearButton.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./GlobalMapYearButton.css";
 
-
 export const GlobalMapYearButton = ( { year, selectedYear, onYearButtonClick } ) => {
     const handleClick = () => {
         onYearButtonClick( year );
+        console.log( year );
     };
+
     return (
-        <button
-            className={`global-map-year-button ${year === selectedYear ? "selected" : ""}`}
-            onClick={handleClick}
-        >
-            {year}
-        </button>
+        <div className="button-container">
+            <button
+                className={`global-map-year-button ${year === selectedYear ? "selected" : ""}`}
+                onClick={handleClick}
+            >
+                {year}
+            </button>
+        </div>
     );
 };
 
-GlobalMapYearButton.propTypes = { year: PropTypes.number, selectedYear: PropTypes.string, onYearButtonClick: PropTypes.func };
+GlobalMapYearButton.propTypes = {
+    year: PropTypes.number,
+    selectedYear: PropTypes.string,
+    onYearButtonClick: PropTypes.func,
+};
