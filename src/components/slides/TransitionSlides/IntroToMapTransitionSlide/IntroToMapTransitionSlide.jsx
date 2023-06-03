c141ae9aa656fa461c1938b70be116e93494f80b
Vincent Flattot <v.flattot@gmail.com>
2023-06-03 17:40:39 +0200

    feat(IntroToMapTransitionSlide): add v0 of text

--- src/components/slides/TransitionSlides/IntroToMapTransitionSlide/IntroToMapTransitionSlide.jsx
+++ src/components/slides/TransitionSlides/IntroToMapTransitionSlide/IntroToMapTransitionSlide.jsx
@@ -4,6 +4,14 @@ import { Slide } from "../../Slide/Slide";
 export const IntroToMapTransitionSlide = () => {
 
     return (
-        <Slide title="Transition 1"><div className="temporary_content"><p>{"We can put whatever html element or component here "}</p></div></Slide>
+        <Slide title="Transition 1">
+            <div className="transition_content">
+                <div className="transition_text">
+                    <p>The first question we asked ourselves concerned the spatial distribution of emergency calls: Are there more calls of a certain type in certain places? For example, are there more emergency calls for road accidents next to a motorway?<br/> 
+            On the following page, you can look at the number of calls for each postcode and filter by date or type of call.
+                    </p>
+                </div>
+            </div>
+        </Slide>
     );
 };
