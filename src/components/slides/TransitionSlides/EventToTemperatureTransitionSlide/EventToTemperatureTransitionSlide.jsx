c5a14668c22f47902fffd4608e796440f060096f
Linda Delacombaz <97972818+lindadelacombaz@users.noreply.github.com>
2023-06-03 23:15:20 +0200

    feat(FilterPanelGlobalMap) Corrected error : could not select township
    * feat(IntroductionSlide): add intro text
    
    * feat(MapToEventTransitionSlide): add v0 of text
    
    * feat(EventToTemperatureTransitionSlide): add v0 of text
    
    * feat(IntroToMapTransitionSlide): add v0 of text
    
    * feat(IntroToMapTransitionSlide) Changed text, added title and increased the size of the font
    
    * feat(xTransitionSlide) Added and Corrected text
    
    * feat(FilterPanelGlobalMap) Corrected error
    
    ---------
    
    Co-authored-by: Vincent Flattot <v.flattot@gmail.com>

--- src/components/slides/TransitionSlides/EventToTemperatureTransitionSlide/EventToTemperatureTransitionSlide.jsx
+++ src/components/slides/TransitionSlides/EventToTemperatureTransitionSlide/EventToTemperatureTransitionSlide.jsx
@@ -4,6 +4,17 @@ import { Slide } from "../../Slide/Slide";
 export const EventToTemperatureTransitionSlide = () => {
 
     return (
-        <Slide title="Transition 3"><div className="temporary_content"><p>{"We can put whatever html element or component here "}</p></div></Slide>
+        <Slide title="Impact of temperature on the 911 calls">
+            <div className="transition_content">
+                <div className="transition_text">
+                    <p>
+                        <span className="bold">{"\""}When it{"'"}s very cold, it freezes, so people are more likely to have road accidents{"\""}. </span> <br/>
+                        This sentence seems quite logical, so we wanted to see if this was really the case in practice. To analyse this, we looked at the temperature in Montgomery County every day at every hour of the day. We then associated each emergency call with the temperature at the time of the call.<br/> 
+                        It is funny to see that, in practice, our first sentence comes true: for all sub-zero temperatures (apart from -10°C), the majority of calls concern vehicles involved in accidents... <br/>
+                        Have fun looking at the majority of calls for each temperature. In the graph, we only display the 10 types of call with the most occurrences so as not to overload with hundreds of bars (there are almost 200 types of emergency call referenced in our dataset, that{"'"}s a lot, don{"'"}t you think? ) 
+                    </p>
+                </div>
+            </div>
+        </Slide>
     );
 };
--- src/components/slides/TransitionSlides/IntroToMapTransitionSlide/IntroToMapTransitionSlide.jsx
+++ src/components/slides/TransitionSlides/IntroToMapTransitionSlide/IntroToMapTransitionSlide.jsx
@@ -4,6 +4,23 @@ import { Slide } from "../../Slide/Slide";
 export const IntroToMapTransitionSlide = () => {
 
     return (
-        <Slide title="Transition 1"><div className="temporary_content"><p>{"We can put whatever html element or component here "}</p></div></Slide>
+        <Slide title="Impact of the geographical location on the number of 911 calls">
+            <div className="transition_content">
+                <div className="transition_text">
+                    <p><span className="bold">Are there regions within the Montgomery County where there are more
+                    911 calls ?</span><br/>
+                    This is the first question we asked ourselves.
+                    We were curious about the spatial distribution of emergency calls. <br/>
+                    For example, are there more calls of a certain type in certain places? <br/>
+                    You will see that the <span className="bold"> Pottstown township </span> is particularly affected by the calls for the majority of the type of accidents.
+                    It is not that surprising since it is the biggest township in term of population density, after Norristown.<br/>
+                    Are there more emergency calls for road accidents next to a motorway?<br/> 
+                    To answer these questions, we have created a map of the county with the number of calls per postcode.
+            You can discover the map on the following page, where we plotted a map of the Montgomery County, PA.<br/>
+            You can look at the number of calls for each postcode and filter by the time (year and month), type of call or the township.
+                    </p>
+                </div>
+            </div>
+        </Slide>
     );
 };
