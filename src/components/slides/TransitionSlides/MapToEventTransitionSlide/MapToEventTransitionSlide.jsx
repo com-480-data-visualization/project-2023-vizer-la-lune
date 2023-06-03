94ee62a7d3ce736a868645b3cbdd3228b7bd44e1
delacombazlinda <delacombaz.linda@gmail.com>
2023-06-03 22:49:14 +0200

    feat(xTransitionSlide) Added and Corrected text

--- src/components/slides/TransitionSlides/MapToEventTransitionSlide/MapToEventTransitionSlide.jsx
+++ src/components/slides/TransitionSlides/MapToEventTransitionSlide/MapToEventTransitionSlide.jsx
@@ -4,10 +4,14 @@ import { Slide } from "../../Slide/Slide";
 export const MapToEventTransitionSlide = () => {
 
     return (
-        <Slide title="Transition 2">
+        <Slide title="Impact of events on 911 calls">
             <div className="transition_content">
                 <div className="transition_text">
-                    <p>Our second question concerns the impact of certain events on emergency calls. Are there more car accidents during SuperBall? 
+                    <p>Our second question concerns the impact of certain events on emergency calls. <br/>
+                        <span className= "bold"> Are there more 911 calls if a big event is taking place in the county ? </span><br/>
+                        For example, are there more car accidents during SuperBall? Can we visualize a decrease of 911 calls during the pandemic ? <br/>
+                        In the next page, you will find a chart of the type of accident (EMS, Fire, Traffic) over a period of time.
+                        You can also filter by the type of event (SuperBowl, Covid-19, etc.) to visualize the impact of the event on the number of calls.
                     </p>
                 </div>
             </div>
